0a8beed67b9ce386b13c57269e36c5ad3fd6b211
david-foy89 <david.foy89@gmail.com>
2026-03-20 12:37:35 -0400

    Add webpack runtime and Vercel feedback script for enhanced performance and user feedback

---
+++ season4_page_chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1211],{},function(n){n.O(0,[9924,6323,3470,5295,790,3223,7302,2437,111,5487,1744],function(){return n(n.s=12437)}),_N_E=n.O()}]);
